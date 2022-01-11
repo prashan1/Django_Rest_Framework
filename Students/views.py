26165f1400cea19f1271407befd334f1843037be
prashan1 <pb257791@gmail.com>
2022-01-12 03:35:20 +0530

    Using Concrete api view  ListCreateAPIview

--- Students/views.py
+++ Students/views.py
@@ -1,32 +1,15 @@
 from .serializers import * 
 from .models import * 
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import (
-                ListModelMixin,
-                RetrieveModelMixin,
-                CreateModelMixin,
-                UpdateModelMixin,
-                DestroyModelMixin,
+from rest_framework.generics import (
+                ListCreateAPIView,
+                RetrieveUpdateDestroyAPIView,
             )
 
 #Aggregating api which don't require pk and have common memebers
-class Student_api_LC(GenericAPIView, ListModelMixin, CreateModelMixin):
+class Student_api_LC(ListCreateAPIView):
     queryset = StudentModel.objects.all()  
     serializer_class = StudentSerialize
 
-    def get(self, request, *args, **kwargs):
-        return self.list( request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create( request, *args, **kwargs)
-
-class Student_api_RUD(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin ):
+class Student_api_RUD(RetrieveUpdateDestroyAPIView):
     queryset = StudentModel.objects.all()  
     serializer_class = StudentSerialize
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve( request, *args, **kwargs)
-    def put(self, request, *args, **kwargs):
-        return self.update( request, *args, **kwargs)
-    def delete(self, request, *args, **kwargs):
-        return self.destroy( request, *args, **kwargs)
